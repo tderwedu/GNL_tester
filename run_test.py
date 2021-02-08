919ec3ed9991144f0642c95cb8bbeacda1cde6d4
tderwedu <tderwedu@student.s19.be>
2021-02-08 18:59:48 +0100

    GNL_t: V0.2 - Bonus part 100%

--- run_test.py
+++ run_test.py
@@ -5,26 +5,30 @@ import os
 
 gnl_path	=	"../getNextLine"
 
-sizes		= [1, 2, 3, 10, 11, 16, 32, 100, 9999, 1000000]
-files		= ["file_00.txt", "file_01.txt", "file_02.txt", "file_03.txt", "file_04.txt"]
-files_b		= ["bonus_0_0.txt", "bonus_0_1.txt", "bonus_0_2.txt"]
-expl		= ["A simple line", "A simple file", "Multiple newlines", "EOF without EOL", "Open() man page"]
-lines		= [1, 3, 8, 1, 653]
+buffSizes	=	[1, 2, 3, 10, 11, 16, 32, 100, 9999, 1000000]
+files		=	["file_00.txt", "file_01.txt", "file_02.txt", "file_03.txt", "file_04.txt",\
+				 "file_05.txt", "file_06.txt"]
+files_b0	= 	["bonus_0_0.txt", "bonus_0_1.txt", "bonus_0_2.txt"]
+files_b1	= 	["bonus_1_0.txt", "bonus_1_1.txt", "bonus_1_2.txt", "bonus_1_3.txt"]
+expl		= 	["A simple line", "A simple file", "Multiple newlines", "EOF without EOL", "Open() man page", \
+				 "Long line with EOL", "Long line w/o EOL"]
+expl_b		=	["Open() 3 files", "Long line w/o EOL"]
+nbLines		=	[1, 3, 8, 1, 653, 1, 1]
 
-norme		= "~/.norminette/norminette.rb"
-name		= "gnl"
+norme		=	"~/.norminette/norminette.rb"
+execName	=	"gnl"
 
-src			= ["get_next_line.c", "get_next_line_utils.c"]
-src_b		= ["get_next_line_bonus.c", "get_next_line_utils_bonus.c"]
+src			=	["get_next_line.c", "get_next_line_utils.c"]
+src_b		=	["get_next_line_bonus.c", "get_next_line_utils_bonus.c"]
 
 
 
-cflags		= "-Wall -Wextra -Werror -fsanitize=address"
-cc			= "gcc"
-main		= "main.c"
-bonus		= "bonus.c"
-result		= "result.txt"
-output		= "STD"
+cflags		=	"-Wall -Wextra -Werror -fsanitize=address"
+cc			=	"gcc"
+main		=	"main.c"
+bonus		=	"bonus.c"
+result		=	"result.txt"
+outputDir	=	"Errors"
 
 BOLD		=	"\033[1m"
 BLU			=	"\033[96m"
@@ -37,47 +41,15 @@ NC			=	"\033[0m"
 src 		= [os.path.join(gnl_path, file) for file in src]
 src_b 		= [os.path.join(gnl_path, file) for file in src_b]
 files		= [os.path.join("Test_Files", file) for file in files]
-files_b		= [os.path.join("Test_Files", file) for file in files_b]
+files_b0	= [os.path.join("Test_Files", file) for file in files_b0]
+files_b1	= [os.path.join("Test_Files", file) for file in files_b1]
 
 ################################################################################
 #                                   Functions                                  #
 ################################################################################
 
-def ft_checkOuput(process, file, lines):
-	if process.returncode:
-		ft_writeIfError(1, process, file)
-		if "Sanitizer" in process.stderr:
-			return BOLD + "|" + NC + PUR + "{:^12}".format("[LEAKS]") + NC
-		else:
-			return BOLD + "|" + NC + RED + "{:^12}".format("[CRASH]") + NC
-	else:
-		if filecmp.cmp(result, file):
-			ret = BOLD + "|" + NC + GRN + "{:^6}".format("[OK]") + NC
-		else:
-			ft_writeIfError(2, process, file)
-			ret = BOLD + "|" + NC + YLW + "{:^6}".format("[FAIL]") + NC
-		if lines == int(process.stderr):
-			ret = ret  + GRN + "{:^6}".format("[OK]") + NC
-		else:
-			ret = ret  + YLW + "{:^6}".format("[FAIL]") + NC
-		return ret
-
-def ft_writeIfError(nb, process, file):
-	if not os.path.isdir(output):
-		os.mkdir(output)
-	if nb == 1:
-		path = os.path.join(output, "STDERR_" + os.path.basename(file))
-		log = open(path, "w", 1)
-		log.write(process.stderr)
-		log.close
-	elif nb == 2:
-		path = os.path.join(output, "STDOUT_" + os.path.basename(file))
-		log = open(path, "w", 1)
-		log.write(process.stdout)
-		log.close
-
-def ft_reshape(vec):
-	i_max = (os.get_terminal_size().columns - 20) // 13
+def ft_reshape(vec, length):
+	i_max = (os.get_terminal_size().columns - 21) // length
 	j_max = m.ceil(len(vec) / i_max)
 	tab		= list()
 	i_s	= 0
@@ -88,61 +60,119 @@ def ft_reshape(vec):
 	tab.append([v for v in vec[i_s:i_s + i_e]])
 	return [tab, j_max]
 
-def ft_mandatoryPart(name, files, sizes):
-	# Creating executables
-	exe_names = list()
-	print(BOLD + "Genereating executables" + NC)
-	for size in sizes:
-		exe_names.append(name + "_" + str(size))
-		bflags	= "-D BUFFER_SIZE=" + str(size)
-		exe		=	cc + " " + cflags + " " + bflags + " -I " + gnl_path + " -o " +\
-					exe_names[-1] + " " + " ".join(src) + " " + main
-		process = subprocess.run(exe.split(), stderr=subprocess.DEVNULL)
-		if  not process.returncode:
-			print(BOLD + "{:<12}: ".format(exe_names[-1]) + NC + GRN + "[OK]" + NC)
-		else:
-			print(BOLD + "{:<12}: ".format(exe_names[-1]) + NC + RED + "[FAIL]" + NC)
-	print("\n")
+def ft_createExec(execName, mainName, sizes):
+	execNames = list()
 	# Reshaping to adapt to terminal size
-	[sizes, j_max] = ft_reshape(sizes)
-	[exe_names, j_max] = ft_reshape(exe_names)
+	[sizes, j_max] = ft_reshape(sizes, 10)
+	print(BOLD + BLU + "Creating executables" + NC)
 	for j in range(j_max):
-		# Printing header
 		print(BOLD + "{:^20}".format("BUFFER_SIZE") + NC, end = "")
 		for size in sizes[j]:
-			print(BOLD + "|{:^12}".format(size) + NC, end = "")
-		print("\n{:20}".format(" "), end = "")
+				print(BOLD + "|{:^9}".format(size) + NC, end = "")
+		print(BOLD + "|" + NC + "\n{:20}".format(" "), end = "")
 		for size in sizes[j]:
-			print(BOLD + "|{:^6}{:^6}".format("Ouput", "Lines") + NC, end = "")
-		print()
+			execNames.append(execName + "_" + str(size))
+			bflags	= "-D BUFFER_SIZE=" + str(size)
+			exe		=	cc + " " + cflags + " " + bflags + " -I " + gnl_path + " -o " +\
+						execNames[-1] + " " + " ".join(src) + " " + mainName
+			process = subprocess.run(exe.split(), stderr=subprocess.DEVNULL)
+			if  not process.returncode:
+				print(BOLD + "|" + NC + GRN + "{:^9}".format("[OK]") + NC, end = "", flush = True)
+			else:
+				print(BOLD + "|" + NC + RED + "{:^9}".format("[FAIL]") + NC, end = "", flush = True)
+		print(BOLD + "|" + NC +"\n")
+	return execNames
+
+def ft_printBuffSizes(sizes, j):
+	print(BOLD + "{:^20}".format("BUFFER_SIZE") + NC, end = "")
+	for size in sizes[j]:
+		print(BOLD + "|{:^12}".format(size) + NC, end = "")
+	print("\n{:20}".format(" "), end = "")
+	for size in sizes[j]:
+		print(BOLD + "|{:^6}{:^6}".format("Ouput", "Lines") + NC, end = "")
+	print(BOLD + "|" + NC )
+
+def ft_checkOuput(process, file, lines, size):
+	if process.returncode:
+		ft_writeIfError(1, process, file, size)
+		if "Sanitizer" in process.stderr:
+			return BOLD + "|" + NC + PUR + "{:^12}".format("[LEAKS]") + NC
+		else:
+			return BOLD + "|" + NC + RED + "{:^12}".format("[CRASH]") + NC
+	else:
+		if filecmp.cmp(result, file):
+			ret = BOLD + "|" + NC + GRN + "{:^6}".format("[OK]") + NC
+		else:
+			ft_writeIfError(2, process, file, size)
+			ret = BOLD + "|" + NC + YLW + "{:^6}".format("[FAIL]") + NC
+		if lines:
+			if lines == int(process.stderr):
+				ret += GRN + "{:^6}".format("[OK]") + NC
+			else:
+				ret += YLW + "{:^6}".format("[FAIL]") + NC
+		else:
+			ret += "{:^6}".format(" ")
+		return ret
+
+def ft_writeIfError(nb, process, file, size):
+	if not os.path.isdir(outputDir):
+		os.mkdir(outputDir)
+	if nb == 1:
+		path = os.path.join(outputDir, "BF_" + size + "_" + os.path.basename(file))
+		log = open(path, "w", 1)
+		log.write(process.stderr)
+		log.close
+	elif nb == 2:
+		path = os.path.join(outputDir, "BF_" + size + "_" + os.path.basename(file))
+		os.rename(result, path)
+
+def ft_checkGNL(execName, testfiles, checkFiles, cFile, nbLines, buffSizes):
+	# Creating executables
+	execNames = ft_createExec(execName, cFile, buffSizes)
+	# Reshaping to adapt to terminal size
+	[buffSizes, j_max] = ft_reshape(buffSizes, 13)
+	[execNames, j_max] = ft_reshape(execNames, 13)
+	for j in range(j_max):
+		print(BOLD + BLU + "Testing GNL" + NC)
+		# Printing BUFFER_SIZES
+		ft_printBuffSizes(buffSizes, j)
 		# Testing and printing result
-		for i in range(len(files)):
+		for i in range(len(testfiles)):
 			print(BOLD + "{:<20}".format(expl[i]) + NC, end="")
-			for n in exe_names[j]:
-				if not os.path.isfile(n):
+			for name in execNames[j]:
+				if not os.path.isfile(name):
 					print( BOLD + "|" + NC + RED + "{:^12}".format("[CRASH]") + NC)
 				log = open(result, "w", 1)
-				process =	subprocess.run(["./" + n, files[i]], \
-							text=True, stdout=log, stderr=subprocess.PIPE)
+				process =	subprocess.run(["./" + name] + testfiles[i], text=True,\
+							stdout = log, stderr = subprocess.PIPE)
 				log.close
-				print(ft_checkOuput(process, files[i], lines[i]), end = "")
+				buffSize = name.split("_")[-1]
+				print(ft_checkOuput(process, checkFiles[i], nbLines[i], buffSize),\
+						end = "", flush = True)
 				os.remove(result)
-			print()
-		print("\n")
+			print(BOLD + "|" + NC)
+		print()
 		# Removing executable files
-		for name in exe_names[j]:
+		for name in execNames[j]:
 			os.remove(name)
+
+def	ft_printHeader(s):
+	l = os.get_terminal_size().columns
+	print(BOLD + BLU + "{:#^{x}}".format("#", x = l))
+	print("#{:^{x}}#".format(s, x = l - 2))
+	print("{:#^{x}}".format("#", x = l) + NC)
 
 ################################################################################
 #                                    Script                                    #
 ################################################################################
 
-bflags	= "-D BUFFER_SIZE=" + str(32)
-exe		=	cc + " " + cflags + " " + bflags + " -I " + gnl_path + " -o " + \
-			name + " " + " ".join(src_b) + " " + bonus
-process = subprocess.run(exe.split(), stderr=subprocess.DEVNULL)
-
-log = open(result, "w", 1)
-process =	subprocess.run(["./" + name] + files_b, text=True, stdout=log, stderr=subprocess.PIPE)
-log.close
-print(process)
+ft_printHeader("MANDATORY PART")
+testfiles	= [[f] for f in files]
+checkFiles	= files
+ft_checkGNL(execName, testfiles, checkFiles, "main.c", nbLines, buffSizes)
+ft_printHeader("BONUS PART")
+testfiles	= [files_b0, files_b1]
+checkFiles	= [files[4], files[6]]
+buffSizes	= [buffSizes[i] for i in range(len(buffSizes)) if not i % 2 ]
+nbLines		= [0] * len(testfiles)
+ft_checkGNL(execName, testfiles, checkFiles, "bonus.c", nbLines, buffSizes)
